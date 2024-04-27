4b07d581d06b9adba699ad39d79ca46f92e454bd
satvik <satvikshukla453@gmail.com>
2024-04-27 12:14:37 +0530

    hero reworked

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,15 @@
 const Hero = () => {
   return (
-    <section className="w-full bg-gray-default overflow-hidden flex flex-row items-start justify-start py-24 px-20 box-border max-w-full shrink-0 text-left text-41xl text-gray-900 font-body2-normal-all mq750:py-[62px] mq750:px-10 mq750:box-border">
-      <div className="flex-1 flex flex-row flex-wrap items-start py-0 px-8 box-border gap-[48px_46px] max-w-full mq750:gap-[23px] justify-center">
-        <div className="w-[768px] overflow-hidden shrink-0 flex flex-col items-start justify-center gap-[48px] max-w-[768px] mq450:gap-[24px] mq1050:max-w-full">
+    <section className="w-full bg-gray-default overflow-hidden flex flex-row items-start justify-start box-border max-w-full shrink-0 text-left text-41xl text-gray-900 font-body2-normal-all py-36 mq1050:py-10 mq750:box-border">
+      <div className="w-full flex flex-row mq1050:flex-col-reverse items-start px-10 box-border gap-[48px_46px] max-w-full mq750:gap-[23px] justify-center">
+        <div className="max-w-[768px] w-full overflow-hidden flex flex-col items-center justify-center gap-[48px] mq450:gap-[24px] mq1050:max-w-full">
           <div className="self-stretch flex flex-col items-start justify-center gap-[8px] max-w-full">
-            <h1 className="m-0 h-[72px] relative text-inherit tracking-[-0.02em] leading-[72px] font-bold font-inherit inline-block max-w-full mq450:text-17xl mq450:leading-[43px] mq1050:text-29xl mq1050:leading-[58px]">
+            <h1 className="m-0 relative text-inherit tracking-[-0.02em] leading-[72px] font-bold font-inherit inline-block max-w-full mq450:text-17xl mq450:leading-[43px] mq1050:text-29xl mq1050:leading-[58px]">
               Hi, I’m Rishabh 👋
             </h1>
             <div className="self-stretch relative text-base leading-[24px] text-gray-600 whitespace-pre-wrap">{`I'm a UX Designer and Web Developer (React.js & Node.js) with a focus on creating exceptional digital experiences that are user-centric, accessible, usable, visually appealing, and responsive. I have been  designing web and android applications for over a year and developing web applications for over a year now, I have a deep passion for designing and researching about user psychology.`}</div>
           </div>
-          <div className="flex flex-col items-start justify-center gap-[8px] text-base text-gray-600">
+          <div className="w-full flex flex-col items-start justify-center gap-[8px] text-base text-gray-600">
             <div className="flex flex-row items-center justify-start py-0 pr-[637px] pl-0 gap-[8px] mq450:pr-5 mq450:box-border mq1050:pr-[318px] mq1050:box-border">
               <img
                 className="h-6 w-6 relative overflow-hidden shrink-0 min-h-[24px]"
@@ -59,7 +59,7 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-row items-start justify-end py-0.5 px-0 box-border min-w-[384px] max-w-full mq450:min-w-full">
+        <div className="mq1050:w-full flex flex-row items-center justify-center py-0.5 px-0 box-border min-w-[384px] max-w-full mq450:min-w-full">
           <div className="h-[360px] w-80 relative">
             <div className="absolute top-[40px] left-[40px] bg-gray-200 box-border w-[280px] h-80 overflow-hidden border-[8px] border-solid border-gray-default" />
             <img
